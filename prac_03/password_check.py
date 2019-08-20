d7acaae1ae6439bbf3381abf912ddd29244bcc3c
BuildTools <unconfigured@null.spigotmc.org>
2019-08-21 07:48:32 +1000

    Add Hide Password Function

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,14 +1,16 @@
 def main():
     password = input("Enter the password \n>>> ")
-    hidden_password = "*" * len(password)
 
-    print("Password: " + hidden_password)
-
+    hide_password(password)
     password_guess(password)
 
 
-def password_guess(password):
+def hide_password(password):
+    hidden_password = "*" * len(password)
+    print("Password: " + hidden_password)
 
+
+def password_guess(password):
     guess = input("Enter the password \n>>> ")
     while guess != password:
         print("Incorrect password. Please try again.")
